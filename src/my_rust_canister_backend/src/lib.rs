6f57ee1ecb063f57914d6d48b8a497f523a5da54
Aaron Schwartz <aaronschwartz@Aarons-MacBook-Pro-2.local>
2024-07-24 11:36:18 -0400

    Initial commit

--- src/my_rust_canister_backend/src/lib.rs
+++ src/my_rust_canister_backend/src/lib.rs
@@ -1,4 +1,28 @@
-#[ic_cdk::query]
-fn greet(name: String) -> String {
-    format!("Hello, {}!", name)
+use candid::{CandidType, Deserialize};
+use ic_cdk::api::time;
+use ic_cdk_macros::{init, update};
+
+#[derive(CandidType, Deserialize, Clone)]
+struct Password {
+    value: String,
+}
+
+#[init]
+fn init() {
+    ic_cdk::print("Password Generator canister initialized.");
+}
+
+#[update]
+fn generate_password(length: u64) -> Password {
+    let charset: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ\
+                           abcdefghijklmnopqrstuvwxyz\
+                           0123456789)(*&^%$#@!~";
+    let mut password = String::new();
+    let mut seed = time() as usize;
+    for _ in 0..length {
+        seed = seed.wrapping_add(seed * 1103515245 + 12345) % charset.len();
+        let idx = seed % charset.len();
+        password.push(charset[idx] as char);
+    }
+    Password { value: password }
 }
